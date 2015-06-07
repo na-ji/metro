346f87a73b61154fec8f0ee306f9489ae7599a3f
na-ji <naji.demolitionman@gmail.com>
2015-06-07 23:56:32 +0200

    Add android platform

---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.info({
+	name: 'Metro',
+	description: 'L\'appli des Rois de Metro',
+	version: '0.0.1'
+});
+
+App.accessRule('*.google.com/*');
+App.accessRule('*.googleapis.com/*');
+App.accessRule('*.gstatic.com/*');
