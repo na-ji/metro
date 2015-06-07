5175e10bf383b1f3072bb115d050741bbc1f5107
na-ji <naji.demolitionman@gmail.com>
2015-06-07 10:54:25 +0200

    Add product form hooks

--- client/templates/products/product_add.js
+++ client/templates/products/product_add.js
@@ -21,3 +21,10 @@ Template.productAdd.helpers({
 		};
 	}
 });
+
+AutoForm.addHooks('insertProductForm', {
+	onSuccess: function(operation, result, template) {
+		Materialize.toast('Votre produit a bien été ajouté !', 4000);
+		Router.go("/products");
+	}
+});
