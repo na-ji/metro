9c50ef9bbe3105f245327f7ae30710f4f866c871
na-ji <naji.demolitionman@gmail.com>
2015-09-19 22:30:43 +0200

    Add caddie selling price & prodit

--- client/templates/caddie/caddie_show.js
+++ client/templates/caddie/caddie_show.js
@@ -1,9 +1,30 @@
+var products_list = {};
+
 function product(id) {
-	return Products.findOne(id);
+	if (products_list[id] === undefined) {
+		products_list[id] = Products.findOne(id);
+	}
+	return products_list[id];
 }
 
 function priceWithVat(product) {
 	return (product.price*(1 + product.vat/100)).toFixed(2);
+}
+
+function totalPrice(products) {
+	for (var i = products.length - 1, total = 0, item; i >= 0; i--) {
+		item  = product(products[i].product);
+		total += products[i].quantity * priceWithVat(item);
+	};
+	return total.toFixed(2);
+}
+
+function totalSellPrice(products) {
+	for (var i = products.length - 1, total = 0, item; i >= 0; i--) {
+		item  = product(products[i].product);
+		total += products[i].quantity * item.unitNumber * item.unitSellPrice;
+	};
+	return total.toFixed(2);
 }
 
 Template.caddieShow.helpers({
@@ -11,10 +32,19 @@ Template.caddieShow.helpers({
 		return product(this.product);
 	},
 	totalPrice: function() {
+		return totalPrice(this.products);
+	},
+	totalSellPrice: function() {
+		return totalSellPrice(this.products);
+	},
+	profit: function() {
+		return (totalSellPrice(this.products) - totalPrice(this.products)).toFixed(2);
+	},
+	totalQuantity: function() {
 		for (var i = this.products.length - 1, total = 0, item; i >= 0; i--) {
-			item = product(this.products[i].product);
-			total   += this.products[i].quantity * priceWithVat(item);
+			item  = product(this.products[i].product);
+			total += this.products[i].quantity * item.unitNumber;
 		};
-		return total.toFixed(2);
+		return total;
 	}
 });
