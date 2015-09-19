c2f86af750abccc0b55a28a6282fabe8584128b0
na-ji <naji.demolitionman@gmail.com>
2015-09-19 22:39:21 +0200

    Update templates

--- client/templates/caddie/caddie_form.js
+++ client/templates/caddie/caddie_form.js
@@ -1,6 +1,6 @@
 Template.caddieForm.helpers({
 	productChoices: function() {
-		var products = Products.find().fetch(),
+		var products = Products.find({}, {sort: {type: 1, name: -1}}).fetch(),
 			choices = [];
 
 		for (var i = products.length - 1; i >= 0; i--) {
--- client/templates/includes/af_forms.js
+++ client/templates/includes/af_forms.js
@@ -1,15 +1,15 @@
 Template.afRadioGroup_metro.inheritsHelpersFrom("afRadioGroup_materialize");
 Template.afRadioGroup_metro.inheritsEventsFrom("afRadioGroup_materialize");
 
-Template.afFileUpload_meteor.inheritsHelpersFrom("afFileUpload");
-Template.afFileUpload_meteor.inheritsEventsFrom("afFileUpload");
-Template.afFileUpload_meteor.inheritsHooksFrom("afFileUpload");
-Template.afFileUpload_meteor.replaces("afFileUpload");
+Template.afFileUpload_metro.inheritsHelpersFrom("afFileUpload");
+Template.afFileUpload_metro.inheritsEventsFrom("afFileUpload");
+Template.afFileUpload_metro.inheritsHooksFrom("afFileUpload");
+Template.afFileUpload_metro.replaces("afFileUpload");
 
-Template.fileThumbImg_meteor.inheritsHelpersFrom("afFileUploadThumbImg");
-Template.fileThumbImg_meteor.inheritsEventsFrom("afFileUploadThumbImg");
-Template.fileThumbImg_meteor.inheritsHooksFrom("afFileUploadThumbImg");
-Template.fileThumbImg_meteor.replaces("afFileUploadThumbImg");
+Template.fileThumbImg_metro.inheritsHelpersFrom("afFileUploadThumbImg");
+Template.fileThumbImg_metro.inheritsEventsFrom("afFileUploadThumbImg");
+Template.fileThumbImg_metro.inheritsHooksFrom("afFileUploadThumbImg");
+Template.fileThumbImg_metro.replaces("afFileUploadThumbImg");
 
 Template.afRadioGroup_metro.helpers({
 	itemAtts: function() {
